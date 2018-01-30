0b426d66b5ed616747918271c7180db5a8ca2c9b
mengjiu999 <1099399203@qq.com>
2018-01-30 21:59:19 +0800

    oc

--- NSDictionary/NSDictionary/main.m
+++ NSDictionary/NSDictionary/main.m
@@ -9,6 +9,12 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
+//1.如何创建
+/*
     NSDictionary *dict = [NSDictionary dictionaryWithObject:@"zhk" forKey:@"name"];
-    NSString *name = [];
+    NSString *name = [dict objectForKey:@"name"];
+    NSLog(@"name = %@",name);
+ */
+//注意key和value是一一对应的
+    
 }
