c4d100b202596bd0bcb3f4a997ae1904331b37b9
mengjiu999 <1099399203@qq.com>
2018-01-30 21:54:52 +0800

    oc

---
+++ NSDictionary/NSDictionary/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NSDictionary
+//
+//  Created by apple on 2018/1/30.
+//  Copyright © 2018年 YuNuo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    NSDictionary *dict = [NSDictionary dictionaryWithObject:@"zhk" forKey:@"name"];
+    NSString *name = [];
+}
