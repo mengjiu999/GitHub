b63b8b82fc960795b7c47656cc89aaf1c46b7eaf
mengjiu999 <1099399203@qq.com>
2018-01-31 22:57:44 +0800

    oc

---
+++ 00042Startup process/Startup process/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Startup process
+//
+//  Created by apple on 2018/1/31.
+//  Copyright © 2018年 YuNuo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+/*
+ 项目的启动过程
+ 1.当项目启动时，会加载一个storyboard，但是加载哪一个storyboard是不确定的，需要自己来制定，选中项目-->general-->Main interface
+ 2.加载storyboard中的控制器，但是加载哪一个控制器也不确定，需要自己来制定：选中storyboard-->拖入控制器-->选中控制器-->右侧第三个按钮（Attributes）-->is initial View Controller
+ */
+}
