3afc4f948e7df5221874a49eb96e3e8447fbdc9b
mengjiu999 <1099399203@qq.com>
2018-02-01 20:10:01 +0800

    UI

--- 00043UI Interface construction/00043UI Interface construction/ViewController.h
+++ 00043UI Interface construction/00043UI Interface construction/ViewController.h
@@ -9,7 +9,16 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
+/*
+ 注意：
+ 1.默认情况下类中的属性是不能和storyboard或者xib中的对象联系起来
+ 2.如果想要联系起来，需要在引用类型前面加IBOutlet
+ IBOutlet：
+ 1.是OC关键字
+ 2.没有实际意义
+ 3.作用：只有属性类型前加IBOutlet才能和storyboard或者xib中的对象联系起来
+ */
+@property (nonatomic,strong) IBOutlet UILabel *mylable;
 
 @end
 
--- 00043UI Interface construction/00043UI Interface construction/ViewController.m
+++ 00043UI Interface construction/00043UI Interface construction/ViewController.m
@@ -24,6 +24,14 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+//注意：一般情况下，控制器的类中的方法不能与storyboard中的事件连线
+//如果想要连线，需要将方法的返回值改成IBAction
+/*
+ IBAction:
+     1.是oc中的关键字
+     2.作用相当于void
+     3.只有返回值是IBAction才能与storyboard相连
+ */
 -(IBAction)redBtnClick
 {
     NSLog(@"%s",__func__);
