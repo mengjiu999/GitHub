3cbbd7be2ef3eef15a6748b2897b85f78923bc0a
mengjiu999 <1099399203@qq.com>
2018-02-04 22:27:09 +0800

    UI

---
+++ 00049Calculator/Calculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculator
+//
+//  Created by apple on 2017/12/21.
+//  Copyright © 2017年 YuNuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 00049Calculator/Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by apple on 2017/12/21.
+//  Copyright © 2017年 YuNuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
