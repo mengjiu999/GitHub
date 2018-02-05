60f61e86549cc72f4252eccabb625bf5a1f77ec6
mengjiu999 <1099399203@qq.com>
2018-02-05 23:16:39 +0800

    UI

--- UIImageView frame/UIImageView frame/ViewController.m
+++ UIImageView frame/UIImageView frame/ViewController.m
@@ -16,7 +16,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    //1.根据图片大小设置frame的方式
+    /*
+    //1.1创建UIImageView对象
+    UIImageView *imageView = [[UIImageView alloc] init];
+    
+    //1.2加载图片
+    UIImage *image = [UIImage imageNamed:@"1"];
+    
+    //1.3设置frame
+    imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+    
+    //1.4设置图片
+    imageView.image = image;
+    
+    //1.5超出imageView的部分剪切
+    imageView.clipsToBounds = YES;
+     */
+    //2.创建UIImageView的同时
+    //添加到控制器中
+    [self.view addSubview:imageView];
 }
 
 
