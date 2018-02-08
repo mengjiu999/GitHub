093d2a279bc070c4007d66620484b72262cfe1bf
mengjiu999 <1099399203@qq.com>
2018-02-08 21:40:09 +0800

    Interface construction

---
+++ 00056Shopping Cart/00056Shopping Cart/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  00056Shopping Cart
+//
+//  Created by apple on 2018/2/8.
+//  Copyright © 2018年 YuNuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
