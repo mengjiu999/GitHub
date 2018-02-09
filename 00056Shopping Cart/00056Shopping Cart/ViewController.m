b8c81bf50020974d7af564ee578348616fc35e60
mengjiu999 <1099399203@qq.com>
2018-02-09 19:40:39 +0800

    UI

--- 00056Shopping Cart/00056Shopping Cart/ViewController.m
+++ 00056Shopping Cart/00056Shopping Cart/ViewController.m
@@ -13,11 +13,13 @@
 @property (weak, nonatomic) IBOutlet UIView *shoppingCartView;
 
 //记录下标值
-@property (nonatomic,assign) NSInteger index;
+//@property (nonatomic,assign) NSInteger index;
 
 //移除商品的引用
 @property (weak, nonatomic) IBOutlet UIButton *removeProductbtn;
 
+//添加商品的引用
+@property (weak, nonatomic) IBOutlet UIButton *addProductbtn;
 
 @end
 
@@ -25,6 +27,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+   // self.index = 0;
 }
 
 
@@ -43,8 +46,9 @@
     //1.2计算常量
     CGFloat hMargin = (self.shoppingCartView.frame.size.width-3 * width)/2;
     CGFloat wMargin = (self.shoppingCartView.frame.size.height - 2 * height);
-    CGFloat x = self.index % 3 * (width + hMargin);
-    CGFloat y = self.index / 3 * (height + wMargin );
+    NSInteger index = self.shoppingCartView.subviews.count;
+    CGFloat x = index % 3 * (width + hMargin);
+    CGFloat y = index / 3 * (height + wMargin );
     
     //创建商品的view
     UIView *productView = [[UIView alloc] init];
@@ -57,17 +61,34 @@
     //添加到购物车
     [self.shoppingCartView addSubview:productView];
     
-    self.index++;
+    //self.index++;
     
     //判断按钮的状态
-    if(self.index == 6){
+    /*
+    if(self.shoppingCartView.subviews.count == 6){
         sender.enabled = NO;
     }
+     */
+    self.removeProductbtn.enabled = self.shoppingCartView.subviews.count != 6;
 }
 
 #pragma mark 移除商品
 - (IBAction)removeProduct:(UIButton *)sender {
- 
+    //移除最后一个商品
+    UIView *lastProduct = [self.shoppingCartView.subviews lastObject];
+    [lastProduct removeFromSuperview];
+    //self.index--;
+    
+    //判断按钮状态
+    /*
+     if(self.shoppingCartView.subviews.count == 0){
+        sender.enabled = NO;
+    }
+     */
+    sender.enabled = self.shoppingCartView.subviews.count;
+    //判断添加按钮的状态
+    self.addProductbtn.enabled = YES;
+    
 }
 
 @end
