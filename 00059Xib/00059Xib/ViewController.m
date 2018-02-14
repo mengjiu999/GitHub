5aed58ceefbede49f80d726f542053aed390d588
mengjiu999 <1099399203@qq.com>
2018-02-14 21:16:43 +0800

    xib

--- 00059Xib/00059Xib/ViewController.m
+++ 00059Xib/00059Xib/ViewController.m
@@ -19,11 +19,11 @@
     //将新闻的View添加到控制器中
     //1.加载出新闻的View
     //1.1.加载方式一（常用）
-    //UIView *newsView = [[[NSBundle mainBundle] loadNibNamed:@"Empty" owner:nil options:nil] firstObject];
+    UIView *newsView = [[[NSBundle mainBundle] loadNibNamed:@"Empty" owner:nil options:nil] firstObject];
     
     //1.2.加载方式二（了解）
-    UINib *nib = [UINib nibWithData:@"Empty" bundle:[NSBundle mainBundle]];
-    UIView *newsView = [[nib instantiateWithOwner:nil options:nil] lastObject];
+    //UINib *nib = [UINib nibWithData:@"Empty" bundle:[NSBundle mainBundle]];
+    //UIView *newsView = [[nib instantiateWithOwner:nil options:nil] lastObject];
     
     //2.添加到控制器中
     [self.view addSubview:newsView];
