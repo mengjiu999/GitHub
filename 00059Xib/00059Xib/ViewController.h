9928eb38241e45f81060396da6f2d79bc71f53e3
mengjiu999 <1099399203@qq.com>
2018-02-14 21:06:28 +0800

    xib

---
+++ 00059Xib/00059Xib/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  00059Xib
+//
+//  Created by apple on 2018/2/14.
+//  Copyright © 2018年 YuNuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 00059Xib/00059Xib/ViewController.m
@@ -0,0 +1,39 @@
+//
+//  ViewController.m
+//  00059Xib
+//
+//  Created by apple on 2018/2/14.
+//  Copyright © 2018年 YuNuo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    //将新闻的View添加到控制器中
+    //1.加载出新闻的View
+    //1.1.加载方式一（常用）
+    //UIView *newsView = [[[NSBundle mainBundle] loadNibNamed:@"Empty" owner:nil options:nil] firstObject];
+    
+    //1.2.加载方式二（了解）
+    UINib *nib = [UINib nibWithData:@"Empty" bundle:[NSBundle mainBundle]];
+    UIView *newsView = [[nib instantiateWithOwner:nil options:nil] lastObject];
+    
+    //2.添加到控制器中
+    [self.view addSubview:newsView];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
