16151523930aabc13abc3d8314a1d2fa9069aa6c
mengjiu999 <1099399203@qq.com>
2018-02-25 23:06:49 +0800

    UI

---
+++ demoLayout/demoLayout/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  demoLayout
+//
+//  Created by William on 2017/12/21.
+//  Copyright © 2017年 William. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ demoLayout/demoLayout/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  demoLayout
+//
+//  Created by William on 2017/12/21.
+//  Copyright © 2017年 William. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
