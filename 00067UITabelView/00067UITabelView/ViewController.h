5cb923328cc5df23d2b8cfff725f0cc7c9d9074e
mengjiu999 <1099399203@qq.com>
2018-03-04 22:29:56 +0800

    UI

---
+++ 00067UITabelView/00067UITabelView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  00067UITabelView
+//
+//  Created by apple on 2018/3/4.
+//  Copyright © 2018年 YuNuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 00067UITabelView/00067UITabelViewTests/_0067UITabelViewTests.m
@@ -0,0 +1,39 @@
+//
+//  _0067UITabelViewTests.m
+//  00067UITabelViewTests
+//
+//  Created by apple on 2018/3/4.
+//  Copyright © 2018年 YuNuo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0067UITabelViewTests : XCTestCase
+
+@end
+
+@implementation _0067UITabelViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
