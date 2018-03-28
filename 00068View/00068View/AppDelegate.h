4e7f69dadcc21ae0a7fb4267ccbda5eabfbe18b5
mengjiu999 <1099399203@qq.com>
2018-03-28 22:56:04 +0800

    UI

---
+++ 00068View/00068View/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  00068View
+//
+//  Created by apple on 2018/3/28.
+//  Copyright © 2018年 YuNuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
