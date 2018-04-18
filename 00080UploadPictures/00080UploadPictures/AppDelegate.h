8590cca14cb6b0680d5e3183e1c031fa984929e1
mengjiu999 <1099399203@qq.com>
2018-04-18 19:02:14 +0800

    UI

---
+++ 00080UploadPictures/00080UploadPictures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  00080UploadPictures
+//
+//  Created by apple on 2018/4/18.
+//  Copyright © 2018年 YuNuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 00080UploadPictures/00080UploadPictures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  00080UploadPictures
+//
+//  Created by apple on 2018/4/18.
+//  Copyright © 2018年 YuNuo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
